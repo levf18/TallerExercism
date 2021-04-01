ebfce1bf7e457527dab495c1b32fe76891f48ebe
luz Villar <luzvf18@gmail.com>
2021-03-31 21:15:05 -0500

    Ejercicio Raindrops terminado

--- raindrops/raindrops.py
+++ raindrops/raindrops.py
@@ -1,2 +1,14 @@
 def convert(number):
-    pass
+    respuesta1 = "Pling"
+    respuesta2 = "Plang"
+    respuesta3 = "Plong"
+    acum = ""
+    if number % 3 == 0:
+        acum += respuesta1
+    if number % 5 == 0:
+        acum += respuesta2
+    if number % 7 == 0:
+        acum += respuesta3
+    if acum == "":
+        acum += str(number)
+    return acum
